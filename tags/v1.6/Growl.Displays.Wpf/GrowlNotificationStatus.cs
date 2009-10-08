7eccb1593a6cddd868a8b3107a11a8edb8734089
frozencow <frozencow@ad70dc6c-91a4-11de-ac11-cbdc8fbd58d0>
2009-10-08 15:01:25 +0000

    Tagged version 1.6.

---
+++ tags/v1.6/Growl.Displays.Wpf/GrowlNotificationStatus.cs
@@ -0,0 +1,15 @@
+namespace Growl.Displays.Wpf
+{
+    public enum GrowlNotificationStatus
+    {
+        Pending,
+        Opening,
+        Opened,
+        Timingout,
+        Clicking,
+        Closing,
+        Timedout,
+        Clicked,
+        Closed
+    }
+}
